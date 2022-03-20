1f78ae340f3b082ec388977ceac9fbd04ab51c34
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-20 17:08:21 -0500

    cree un componeste jsx, elimine archivos que no usaré

---
+++ src/components/Testimonio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Testimonio() {
+  return (
+    <section className="contenedor-testimonio">
+      <img src={require("../images/testimonio-emma.png")}
+        alt="Foto de Emma" 
+        className="imagen-testimonio"/>
+    </section>
+  );
+};
