0497b9aacbd5941b763d9b64f55de49619f1ccad
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-20 18:12:45 -0500

    terminado el clon de testimonios de freecodecamp en español 😊😊😊

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import '../styles/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <section className="contenedor-testimonio">
-      <img src={require("../images/testimonio-emma.png")}
+      <img 
+        src={require(`../images/testimonio-${props.imagen}.png`)}
         alt="Foto de Emma" 
         className="imagen-testimonio"/>
+      <section className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">
+          "{props.testimonio}".
+        </p>
+      </section>
     </section>
   );
 };
+
+export default Testimonio;
